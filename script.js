ece3a2da72628f60e7cf03759325a13b2c4e6130
Lucas Melo <lucas.melo044@gmail.com>
2022-04-05 16:31:57 -0300

    criando o objeto calculator

--- script.js
+++ script.js
@@ -1,90 +1,32 @@
-const currentOperand = document.querySelector('[data-current]');
-const previousOperand = document.querySelector('[data-previous]');
+const currentOperandTextElement = document.querySelector('[data-current]');
+const previousOperandTextElement = document.querySelector('[data-previous]');
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
+class Calculator {
+  constructor(currentOperandTextElement, previousOperandTextElement) {
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.previousOperandTextElement = previousOperandTextElement;
 
-let expression = [];
-let a = '';
-let b = '';
+  }
 
-deleteButton.addEventListener('click', deleteExpression); // Delete number
+  clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+  }
 
-allClearButton.addEventListener('click', allClearDipslay); // All clear
+  updateDisplay() {
+    this.currentOperandTextElement.innerHTML = this.currentOperand;
+    this.previousOperandTextElement.innerHTML = `${this.previousOperand}  ${this.operation || ''}`;
+  }
+}
 
-equalsButton.addEventListener('click', calculate); // Equals
+const calculator = new Calculator(currentOperandTextElement, previousOperandTextElement);
 
-operatorButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    addExpression(button.innerText);
-    addExpressionPrevious()    
-  })
+allClearButton.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
 })
-
-numberButtons.forEach(numberButton => {
-  numberButton.addEventListener('click', () => {
-    let value = numberButton.innerText;
-    addExpression(value);
-  })
-});
-
-
-function addExpression(value) {
-  expression.push(value);
-
-  if(expression.length != 1) {
-    a = ''
-    showValue();
-  }
-
-  joinExpression();
-  showValue();
- 
-}
-
-function addExpressionPrevious() {
-  b = a;
-  a = '';
-  showValue();
-}
-
-function deleteExpression() {
-  expression.pop();
-
-  a = ''
-
-  joinExpression()
-
-}
-
-function joinExpression() {
- 
-  for(let i of expression) {
-    a += i;
-  }
-
-  showValue();
-}
-
-function allClearDipslay() {
-  a = '';
-  b = '';
-  expression = [];
-  ans = expression;
-  showValue();
-}
-
-function showValue() {
-  currentOperand.innerText = a;
-  previousOperand.innerText = b;
-}
-
-function calculate() {
-  let result = eval(expression.join(''));
-  result;
-  expression = [];
-  expression.push(result);
-  currentOperand.innerHTML = result;
-}
