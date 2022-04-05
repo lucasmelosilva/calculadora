67b82033c92015d0cd404c831556a1a68af79146
Lucas Melo <lucas.melo044@gmail.com>
2022-04-05 13:27:48 -0300

    add delete

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ const allClearButton = document.querySelector('[data-all-clear]');
 let expression = [];
 let a = '';
 
+deleteButton.addEventListener('click', deleteExpression);
+
 numberButtons.forEach(numberButton => {
   numberButton.addEventListener('click', () => {
     let value = numberButton.innerText;
     addExpression(value);
+    console.log(expression);
   })
 });
 
@@ -25,11 +28,26 @@ function addExpression(value) {
     currentOperand.innerText = a;
   }
 
+  joinExpression();
+ 
+}
+
+
+function deleteExpression() {
+  expression.pop();
+  console.log(expression);
+
+  a = ''
+
+  joinExpression()
+  
+  currentOperand.innerText = a;
+}
+
+function joinExpression() {
+ 
   for(let i of expression) {
     a += i;
   }
-
   currentOperand.innerText = a;
-  console.log(expression);
 }
-
